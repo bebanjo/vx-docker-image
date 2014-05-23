21085ab5cf4b62e7a7386fd1101c65a486cef9c9
Paco Guzman <pacoguzmanp@gmail.com>
2014-05-23 07:55:07 +0200

    Setup bebanjo-vexor docker image

--- build.sh
+++ build.sh
@@ -9,8 +9,11 @@ case $1 in
   "precise-full")
     packer build packer/precise-full.json
     ;;
+  "bebanjo-vexor")
+    PACKER_LOG=1 packer build packer/bebanjo-vexor.json
+    ;;
   *)
-    echo "Usage $0 (precise|precise-full)"
+    echo "Usage $0 (precise|precise-full|bebanjo-vexor)"
     exit 1
     ;;
 esac
