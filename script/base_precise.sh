39b44bd709c0fe10ce9afa00400c65aab87bd872
Paco Guzman <pacoguzmanp@gmail.com>
2014-05-27 08:51:13 +0200

    Use specific uid for the user that run the inside the images so we can share volumes properly (the worker machines has to use the same uid)

--- script/base_precise.sh
+++ script/base_precise.sh
@@ -17,8 +17,8 @@ sed -i 's/start on local-filesystems/start on local-filesystems or dockerboot/' 
 # change /bin/mknod, need for openjdk-7-jdk => fuse
 dpkg-divert --local --rename --add /sbin/mknod && ln -s /bin/true /sbin/mknod
 
-# create user
-useradd -m vexor -s /bin/bash
+# create user with specific uid and gid to share properly docker volumes
+useradd -m vexor -s /bin/bash --uid 999
 echo "vexor:vexor" | chpasswd
 apt-get -qy install sudo
 echo "vexor ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers
@@ -39,27 +39,6 @@ locale-gen en_US.UTF-8
 dpkg-reconfigure -fnoninteractive locales
 update-locale LC_ALL="en_US.UTF-8" LANG="en_US.UTF-8" LANGUAGE="en_US"
 
-# There is a conflict with the package versions (maybe is not needed)
-# python : Depends: python2.7 (>= 2.7.3) but it is not going to be installed
-# Depends: python-minimal (= 2.7.3-0ubuntu2) but 2.7.3-0ubuntu2.2 is to be installed
-# python-minimal : Depends: python2.7-minimal (>= 2.7.3) but it is not going to be installed
-
-dpkg --remove --force-all python
-dpkg --remove --force-all python2.7-minimal
-
-wget http://cz.archive.ubuntu.com/ubuntu/pool/main/p/python-defaults/python_2.7.3-0ubuntu2.2_amd64.deb
-wget http://cz.archive.ubuntu.com/ubuntu/pool/main/p/python-defaults/python-minimal_2.7.3-0ubuntu2.2_amd64.deb
-dpkg --install --force-all python-minimal_2.7.3-0ubuntu2.2_amd64.deb
-dpkg --install --force-all python_2.7.3-0ubuntu2.2_amd64.deb
-
-#apt-get autoremove
-apt-get clean
-apt-get autoclean
-apt-get update
-apt-get install -qyf
-
-# install python and modules, needed for ansible
-apt-get install -qy python-minimal
 apt-get install -qy python
 
 #ENTRYPOINT ["/sbin/init", "--startup-event", "dockerboot"]
