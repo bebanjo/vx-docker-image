8a01063967d9ecad3c23a0cbdd43cd75189a47a3
Paco Guzman <pacoguzmanp@gmail.com>
2014-06-12 09:33:22 +0200

    Namespace build command

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@ set -e
 
 case $1 in
   "precise")
-    packer build packer/precise.json
+    packer build packer/docker/precise.json
     ;;
   "precise-full")
-    packer build packer/precise-full.json
+    packer build packer/docker/precise-full.json
     ;;
   "precise-vexor")
-    PACKER_LOG=1 packer build packer/precise-full-vexor.json
+    PACKER_LOG=1 packer build packer/docker/precise-full-vexor.json
     ;;
   *)
     echo "Usage $0 (precise|precise-full|precise-vexor)"
